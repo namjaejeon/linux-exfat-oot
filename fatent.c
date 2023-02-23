1ab1500bb827788235031cd03a498a0daaaca5b2
Yuezhang Mo <Yuezhang.Mo@sony.com>
2023-02-23 23:08:54 +0900

    exfat: don't print error log in normal case
    When allocating a new cluster, exFAT first allocates from the
    next cluster of the last cluster of the file. If the last cluster
    of the file is the last cluster of the volume, allocate from the
    first cluster. This is a normal case, but the following error log
    will be printed. It makes users confused, so this commit removes
    the error log.
    
    [1960905.181545] exFAT-fs (sdb1): hint_cluster is invalid (262130)
    
    Signed-off-by: Yuezhang Mo <Yuezhang.Mo@sony.com>
    Reviewed-by: Andy Wu <Andy.Wu@sony.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- fatent.c
+++ fatent.c
@@ -357,12 +357,16 @@ int exfat_alloc_cluster(struct inode *inode, unsigned int num_alloc,
 		}
 	}
 
-	/* check cluster validation */
-	if (!is_valid_cluster(sbi, hint_clu)) {
-		exfat_err(sb, "hint_cluster is invalid (%u)",
-			hint_clu);
+	if (hint_clu == sbi->num_clusters) {
 		hint_clu = EXFAT_FIRST_CLUSTER;
 		p_chain->flags = ALLOC_FAT_CHAIN;
+	}
+
+	/* check cluster validation */
+	if (!is_valid_cluster(sbi, hint_clu)) {
+		exfat_err(sb, "hint_cluster is invalid (%u)", hint_clu);
+		ret = -EIO;
+		goto unlock;
 	}
 
 	p_chain->dir = EXFAT_EOF_CLUSTER;
